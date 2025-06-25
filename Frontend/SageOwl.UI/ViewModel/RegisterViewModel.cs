8aae6b4f53a332a4bd184b75f0b312a5921d2cd1
alejandroG <alejvo05@gmail.com>
2025-06-25 16:29:07 -0500

    Account Pages Added

---
+++ Frontend/SageOwl.UI/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+namespace SageOwl.UI.ViewModel;
+
+public class RegisterViewModel
+{
+    //Name
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Surname { get; set; }
+
+    //Birth
+    public int Day {  get; set; }
+    public int Month { get; set; }
+    public int Year { get; set; }
+
+    //Password
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+
+    public DateTime BirthDay => new(Year, Month, Day);
+
+}
