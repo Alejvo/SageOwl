fd8f59cf099d1bec193365b0297cd5b17ae2e73d
alejandroG <alejvo05@gmail.com>
2025-06-25 17:17:24 -0500

    Register and Home page minor UI corrections

---
+++ Frontend/SageOwl.UI/ViewModel/PlanViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SageOwl.UI.ViewModel;
+
+public class PlanViewModel
+{
+    public required string Title { get; set; }
+    public required List<string> Characteristics { get; set; }
+    public decimal? MonthlyCost { get; set; }
+    public decimal? YearlyCost { get; set; }
+}
