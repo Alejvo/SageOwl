9c7fced1b961e20797c88b095118d44bea84cd2e
alejandroG <alejvo05@gmail.com>
2025-08-03 13:59:36 -0500

    Added Team Description Page

---
+++ Frontend/SageOwl.UI/Controllers/TeamController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SageOwl.UI.Controllers;
+
+[Route("team/{teamId}")]
+public class TeamController : Controller
+{
+    [HttpGet("mainpage")]
+    public IActionResult MainPage(string teamId)
+    {
+        ViewBag.TeamId = teamId;
+        return View();
+    }
+
+    [HttpGet("qualifications")]
+    public IActionResult Qualifications(string teamId)
+    {
+        ViewBag.TeamId = teamId;
+        return View();
+    }
+
+    [HttpGet("description")]
+    public IActionResult Description(string teamId)
+    {
+        ViewBag.TeamId = teamId;
+        return View();
+    }
+
+    [HttpGet("forms")]
+    public IActionResult Forms(string teamId)
+    {
+        ViewBag.TeamId = teamId;
+        return View();
+    }
+}
