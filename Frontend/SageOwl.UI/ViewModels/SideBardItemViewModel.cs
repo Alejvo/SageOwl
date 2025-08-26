e79496783dcb85a74695c2cfb1dd97fb7ee71481
alejandroG <alejvo05@gmail.com>
2025-08-25 21:45:58 -0500

    Added SideMenu responsive layout

--- Frontend/SageOwl.UI/ViewModels/SideBardItemViewModel.cs
+++ Frontend/SageOwl.UI/ViewModels/SideBardItemViewModel.cs
@@ -1,7 +1,8 @@
-﻿namespace SageOwl.UI.ViewModel;
+﻿namespace SageOwl.UI.ViewModels;
 
 public class SideBardItemViewModel
 {
     public string IconCode { get; set; }
     public string Text { get; set; }
+    public string Url { get; set; }
 }
