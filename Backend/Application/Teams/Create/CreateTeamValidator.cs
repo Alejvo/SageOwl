2620339d298977f163dac6490e56b5f6b5b347a6
alejandroG <alejvo05@gmail.com>
2025-11-27 15:15:18 -0500

    Added team validators

---
+++ Backend/Application/Teams/Create/CreateTeamValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Application.Teams.Create;
+
+public class CreateTeamValidator : AbstractValidator<CreateTeamCommand>
+{
+    public CreateTeamValidator()
+    {
+        RuleFor(t => t.Name).NotEmpty();
+
+        RuleFor(t => t.Description).NotEmpty();
+
+        RuleFor(t => t.Members).NotNull();
+    }
+}
---
+++ Backend/Application/Teams/Update/UpdateTeamValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Application.Teams.Update;
+
+public class UpdateTeamValidator : AbstractValidator<UpdateTeamCommand>
+{
+    public UpdateTeamValidator()
+    {
+        RuleFor(t => t.TeamId).NotEmpty();
+
+        RuleFor(t => t.Name).NotEmpty();
+
+        RuleFor(t => t.Description).NotEmpty();
+
+        RuleFor(t => t.Members).NotNull();
+    }
+}
