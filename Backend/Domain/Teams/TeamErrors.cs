2d81637d7a8a519e6d8fb2a7d921880d9ce1a32f
alejandroG <alejvo05@gmail.com>
2025-11-30 18:18:29 -0500

    Added team errors

---
+++ Backend/Domain/Teams/TeamErrors.cs
@@ -0,0 +1,11 @@
+﻿using Shared;
+
+namespace Domain.Teams;
+
+public class TeamErrors
+{
+    public static Error NotFound => new("Team.NotFound","The team was not found",ErrorType.NotFound);
+    public static Error NoMembersAdded => new("Team.NoMembersAdded", "You have to add members when you create a team", ErrorType.Conflict);
+    public static Error NoAdminAdded => new("Team.NoAdminAdded", "You have to add al least one admin when you create a team", ErrorType.Conflict);
+
+}
