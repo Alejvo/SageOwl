aa12081b7b7756de31e690f51fd5c740a9c8fede
alejandroG <alejvo05@gmail.com>
2026-03-18 18:26:22 -0500

    Added Redis Resilence

--- Backend/Infrastructure/Caching/RedisCacheService.cs
+++ Backend/Infrastructure/Caching/RedisCacheService.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Interfaces;
+using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
 using System.Text.Json;
 
@@ -7,31 +8,59 @@ namespace Infrastructure.Caching;
 public class RedisCacheService : ICacheService
 {
     private readonly IDatabase _database;
+    private readonly ILogger<RedisCacheService> _logger;
 
-    public RedisCacheService(IConnectionMultiplexer redis)
+    public RedisCacheService(IConnectionMultiplexer redis, ILogger<RedisCacheService> logger)
     {
         _database = redis.GetDatabase();
+        _logger = logger;
     }
 
     public async Task<T?> GetAsync<T>(string key)
     {
-        var value = await _database
-            .StringGetAsync(key);
+        try
+        {
+            var value = await _database
+                .StringGetAsync(key);
 
-        if (value.IsNullOrEmpty)
-            return default;
+            if (value.IsNullOrEmpty)
+                return default;
 
-        return JsonSerializer.Deserialize<T>(value!);
+            return JsonSerializer.Deserialize<T>(value!);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogWarning(ex, "Redis error retrieving the {Key}:", key);
+            return default; 
+        }
+
     }
 
     public async Task RemoveAsync(string key)
-         => await _database
+    {
+        try
+        {
+            await _database
                 .KeyDeleteAsync(key);
+        }
+        catch(Exception ex)
+        {
+            _logger.LogWarning(ex, "Redis error deleting the {Key}:", key);
+        }
+    }
 
      public async Task SetAsync<T>(string key,T value, TimeSpan expiryTime)
      {
-        var json = JsonSerializer.Serialize(value);
-        await _database
-            .StringSetAsync(key, json,expiryTime);
-     }
+        try
+        {
+            var json = JsonSerializer.Serialize(value);
+            await _database
+                .StringSetAsync(key, json, expiryTime);
+        }
+        catch (Exception ex) 
+        {
+            _logger.LogWarning(ex, "Redis error while setting the {Key}:", key);
+        }
+
+    }
 }
