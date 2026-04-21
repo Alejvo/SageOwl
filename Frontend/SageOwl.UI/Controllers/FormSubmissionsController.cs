13e085b64be4d26cb34f7f152525859a30550f95
alejandroG <alejvo05@gmail.com>
2026-04-21 16:46:46 -0500

    Added FormSubmission Controller

---
+++ Frontend/SageOwl.UI/Controllers/FormSubmissionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SageOwl.UI.Services.Interfaces;
+using SageOwl.UI.ViewModels.FormSubmissions;
+
+namespace SageOwl.UI.Controllers;
+
+public class FormSubmissionsController : Controller
+{
+    private readonly IFormSubmissionService _formSubmissionService;
+
+    public FormSubmissionsController(IFormSubmissionService formSubmissionService)
+    {
+        _formSubmissionService = formSubmissionService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateFormSubmissionViewModel createFormSubmission)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(createFormSubmission);
+        }
+
+        await _formSubmissionService.CreateFormSubmission(createFormSubmission);
+
+        return View();
+    }
+}
