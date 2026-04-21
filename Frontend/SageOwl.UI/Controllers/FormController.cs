3b43dc90d6aec22a45faa6cb7346ff332bd263a7
alejandroG <alejvo05@gmail.com>
2026-04-21 16:57:20 -0500

    Modified Form Controller

--- Frontend/SageOwl.UI/Controllers/FormController.cs
+++ Frontend/SageOwl.UI/Controllers/FormController.cs
@@ -1,44 +1,63 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SageOwl.UI.Services.Interfaces;
+using SageOwl.UI.ViewModels.Forms.Create;
+using SageOwl.UI.ViewModels.Forms.Update;
 
 namespace SageOwl.UI.Controllers;
 
 public class FormController : Controller
 {
+    private readonly IFormService _formService;
+
+    public FormController(IFormService formService)
+    {
+        _formService = formService;
+    }
+
     // GET Methods
     [Route("form/{formId}")]
-    public IActionResult Index(string formId)
+    public async Task<IActionResult> Index(Guid formId)
     {
         ViewBag.FormId = formId;
         ViewData["HeaderTitle"] = "Form";
         ViewData["HeaderUrl"] = Url.Action("Index", "Workspace");
-        return View();
-    }
 
-    [Route("form/create")]
-    public IActionResult Create()
-    {
-        ViewData["HeaderTitle"] = "Create Form";
-        ViewData["HeaderUrl"] = Url.Action("Index", "Workspace"); 
-        return View();
-    }
-
-    [Route("form/{formId}/update")]
-    public IActionResult Update(string formId)
-    {
-        ViewBag.FormId = formId;
-        ViewData["HeaderTitle"] = "Update Form";
-        ViewData["HeaderUrl"] = Url.Action("Index", "Workspace"); 
-        return View();
-    }
-
-    [Route("form/{formId}/results")]
-    public IActionResult Results(string formId)
-    {
-        ViewBag.FormId = formId;
-        ViewData["HeaderTitle"] = "Results";
-        ViewData["HeaderUrl"] = Url.Action("Index", "Workspace"); 
-        return View();
+        var form = await _formService.GetFormById(formId);
+        return View(form);
     }
 
     // POST Methods
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateFormViewModel createForm)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(createForm);
+        }
+
+        await _formService.CreateForm(createForm);
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(UpdateFormViewModel updateForm)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(updateForm);
+        }
+
+        await _formService.UpdateForm(updateForm);
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(Guid formId)
+    {
+        if (formId == Guid.Empty)
+            return View();
+
+        await _formService.DeleteForm(formId);
+        return View();
+    }
 }
