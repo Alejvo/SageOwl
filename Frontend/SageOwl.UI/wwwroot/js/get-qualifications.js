11b9c9fa247c00f25eff349c2d9da500d3e01d1b
alejandroG <alejvo05@gmail.com>
2026-05-12 16:29:51 -0500

    Modified JS Code

---
+++ Frontend/SageOwl.UI/wwwroot/js/get-qualifications.js
@@ -0,0 +1,16 @@
+﻿async function loadProduct(id) {
+
+    const response = await fetch(`/qualification/QualificationPartial?id=${id}`);
+
+    const html = await response.text();
+
+    document.getElementById("qualificationContainer").innerHTML = html;
+}
+
+function search() {
+
+    const id =
+        document.getElementById("periodSelect").value;
+
+    loadProduct(id);
+}
